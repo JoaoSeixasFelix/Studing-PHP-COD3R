f016e09ecce0998cac156203de893748277e7e14
Joao Paulo <joaopaulo_seixas@outlook.com>
2022-10-24 23:53:56 -0300

    updating the project, and learning about variables in php - version 17

--- control/if_else.php
+++ control/if_else.php
@@ -7,3 +7,29 @@
 if(true){
     echo "Hello My Friend!";
 }
+
+if (!true){
+    echo "<br> True - Part A";
+    echo "<br> True - Part B";
+} else {
+    echo "<br>False  - Part A";
+    echo "<br>False  - Part B";
+};
+
+
+if (false){
+    echo "<br>Step A<br>";
+} else if(false){
+    echo "<br>Step B<br>";
+} else if(false){
+    echo "<br>Step C<br>";
+} else if(false){
+    echo "<br>Step D<br>";
+} else if(false){
+    echo "<br>Step E<br>";
+} else {
+    echo "<br>Last Step <br>";
+};
+
+
+echo "End <br>";
