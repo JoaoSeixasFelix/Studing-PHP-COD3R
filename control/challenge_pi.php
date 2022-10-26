dce0bd34028bceef745221cd33e47f4abdf998dd
Joao Paulo <joaopaulo_seixas@outlook.com>
2022-10-26 00:54:26 -0300

    updating the project, and learning about variables in php - version 21

--- control/challenge_pi.php
+++ control/challenge_pi.php
@@ -6,9 +6,12 @@
 
 $pi = 3.14;
 $invalidNumber = pi() - $pi;
+echo pi();
 
+/*It was considered in the creation of the parameters, 
+a percentage factor of 5% for the informed values of PI */
 
-if ($invalidNumber <= 0.01){
+if ($invalidNumber < 0.157079633 && $invalidNumber > -0.157079633 ){
       echo "<br> Equals";
 } else {
     echo "<br> Not Equals";
